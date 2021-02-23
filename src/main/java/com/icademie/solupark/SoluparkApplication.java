4432b7d379d267f54ea32825f7e815b8e4469f77
kati-totti <taoufiq.elkati@gmail.com>
2021-02-23 21:32:06 +0100

    first commit

---
+++ src/main/java/com/icademie/solupark/SoluparkApplication.java
@@ -0,0 +1,13 @@
+package com.icademie.solupark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoluparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoluparkApplication.class, args);
+	}
+
+}
